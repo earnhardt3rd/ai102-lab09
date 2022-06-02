49aa7d0f659b5a0d9270dfecc34a0d16fb4c3c1c
Frank <frank.earnhardt@outlook.com>
2022-06-02 12:26:15 -0700

    2022_06_02-LAB19-C

--- AI-102-AIEngineer/19-face/C-Sharp/face-api/Program.cs
+++ AI-102-AIEngineer/19-face/C-Sharp/face-api/Program.cs
@@ -133,7 +133,68 @@ namespace analyze_faces
         static async Task CompareFaces(string image1, string image2)
         {
             Console.WriteLine($"Comparing faces in {image1} and {image2}");
+            // Determine if the face in image 1 is also in image 2
+            DetectedFace image_i_face;
+            using (var image1Data = File.OpenRead(image1))
+            {    
+                // Get the first face in image 1
+                var image1_faces = await faceClient.Face.DetectWithStreamAsync(image1Data);
+                if (image1_faces.Count > 0)
+                {
+                    image_i_face = image1_faces[0];
+                    Image img1 = Image.FromFile(image1);
+                    Graphics graphics = Graphics.FromImage(img1);
+                    Pen pen = new Pen(Color.LightGreen, 3);
+                    var r = image_i_face.FaceRectangle;
+                    Rectangle rect = new Rectangle(r.Left, r.Top, r.Width, r.Height);
+                    graphics.DrawRectangle(pen, rect);
+                    String output_file = "face_to_match.jpg";
+                    img1.Save(output_file);
+                    Console.WriteLine(" Results saved in " + output_file); 
 
+                    //Get all the faces in image 2
+                    using (var image2Data = File.OpenRead(image2))
+                    {    
+                        var image2Faces = await faceClient.Face.DetectWithStreamAsync(image2Data);
+
+                        // Get faces
+                        if (image2Faces.Count > 0)
+                        {
+
+                            var image2FaceIds = image2Faces.Select(f => f.FaceId).ToList<Guid?>();
+                            var similarFaces = await faceClient.Face.FindSimilarAsync((Guid)image_i_face.FaceId,faceIds:image2FaceIds);
+                            var similarFaceIds = similarFaces.Select(f => f.FaceId).ToList<Guid?>();
+
+                            // Prepare image for drawing
+                            Image img2 = Image.FromFile(image2);
+                            Graphics graphics2 = Graphics.FromImage(img2);
+                            Pen pen2 = new Pen(Color.LightGreen, 3);
+                            Font font2 = new Font("Arial", 4);
+                            SolidBrush brush2 = new SolidBrush(Color.Black);
+
+                            // Draw and annotate each face
+                            foreach (var face in image2Faces)
+                            {
+                                if (similarFaceIds.Contains(face.FaceId))
+                                {
+                                    // Draw and annotate face
+                                    var r2 = face.FaceRectangle;
+                                    Rectangle rect2 = new Rectangle(r2.Left, r2.Top, r2.Width, r2.Height);
+                                    graphics2.DrawRectangle(pen2, rect2);
+                                    string annotation = "Match!";
+                                    graphics2.DrawString(annotation,font2,brush2,r2.Left, r2.Top);
+                                }
+                            }
+
+                            // Save annotated image
+                            String output_file2 = "matched_faces.jpg";
+                            img2.Save(output_file2);
+                            Console.WriteLine(" Results saved in " + output_file2);   
+                        }
+                    }
+
+                }
+            }
 
         }
 
