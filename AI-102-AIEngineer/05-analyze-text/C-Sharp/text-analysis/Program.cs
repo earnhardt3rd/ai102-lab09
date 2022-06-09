e1ceb48e8357a637001d3ab157d1511cf7d61b0e
Frank <frank.earnhardt@outlook.com>
2022-06-09 05:59:40 -0700

    2022_06_09-B

--- AI-102-AIEngineer/05-analyze-text/C-Sharp/text-analysis/Program.cs
+++ AI-102-AIEngineer/05-analyze-text/C-Sharp/text-analysis/Program.cs
@@ -45,18 +45,49 @@ namespace text_analysis
                     Console.WriteLine("\n" + text);
 
                     // Get language
+                    DetectedLanguage detectedLanguage = CogClient.DetectLanguage(text);
+                    Console.WriteLine($"\nLanguage: {detectedLanguage.Name}");
 
 
                     // Get sentiment
+                    DocumentSentiment sentimentAnalysis = CogClient.AnalyzeSentiment(text);
+                    Console.WriteLine($"\nSentiment: {sentimentAnalysis.Sentiment}");
 
 
                     // Get key phrases
+                    KeyPhraseCollection phrases = CogClient.ExtractKeyPhrases(text);
+                    if (phrases.Count > 0)
+                    {
+                        Console.WriteLine("\nKey Phrases:");
+                        foreach(string phrase in phrases)
+                        {
+                            Console.WriteLine($"\t{phrase}");
+                        }
+                    }
 
 
                     // Get entities
+                    CategorizedEntityCollection entities = CogClient.RecognizeEntities(text);
+                    if (entities.Count > 0)
+                    {
+                        Console.WriteLine("\nEntities:");
+                        foreach(CategorizedEntity entity in entities)
+                        {
+                            Console.WriteLine($"\t{entity.Text} ({entity.Category})");
+                        }
+                    }
 
 
                     // Get linked entities
+                    LinkedEntityCollection linkedEntities = CogClient.RecognizeLinkedEntities(text);
+                    if (linkedEntities.Count > 0)
+                    {
+                        Console.WriteLine("\nLinks:");
+                        foreach(LinkedEntity linkedEntity in linkedEntities)
+                        {
+                            Console.WriteLine($"\t{linkedEntity.Name} ({linkedEntity.Url})");
+                        }
+                    }
 
 
                 }
